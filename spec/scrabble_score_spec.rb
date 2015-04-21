2be377d7cd99f347034d36d799e058c7dd1cbc1e
Sunny Zhang & Ian Bevel <student@epicodus.com>
2015-04-21 15:04:23 -0700

    final scrabble_score

---
+++ spec/scrabble_score_spec.rb
@@ -0,0 +1,9 @@
+#scrabble_score will total up all letter values of a word based upon scrabble scores
+require('rspec')
+require('scrabble_score')
+
+describe('String#scrabble_score') do
+  it("the word 'at' will return 2") do
+  expect("a".scrabble_score()).to(eq(1))
+  end
+end
